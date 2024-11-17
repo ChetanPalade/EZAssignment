d6f59b0b2adf2474d6ac21896bbeaa5dbcc06e92
ChetanPalade <147688599+ChetanPalade@users.noreply.github.com>
2024-11-17 18:17:55 +0530

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,4 @@
-const User = require('../models/user'); // User model
+const User = require('../models/User'); // User model
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const nodemailer = require('nodemailer');
